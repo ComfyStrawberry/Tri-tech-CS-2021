828a88700c531feff7936d918098fb92bdb6d88a
ComfyStrawberry <72462624+ComfyStrawberry@users.noreply.github.com>
2021-06-14 11:49:32 -0700

    Add files via upload

---
+++ tritech-cyber/Ccode/helloworld.c
@@ -0,0 +1,9 @@
+//helloworld.c
+//compile : gcc helloworld.c -o helloworld.o
+#include <stdio.h>
+int main(){
+	printf("Hello World\n");
+	printf("Hola Mundo\n");
+	printf("AAAAAAAAAAAAAAAAAAAAAAAAAA");
+	return 0;
+}
---
+++ tritech-cyber/Ccode/initials.cpp
@@ -0,0 +1,12 @@
+// g++ initials.cpp -o ccm.o
+#include <iostream>
+using namespace std;
+int main(){
+	cout<< " CC     M M     CC  \n";
+	cout<< "C  C   M M M   C  C \n";
+	cout<< "C     M     M  C    \n";
+	cout<< "C  C           C  C \n";
+	cout<< " CC             CC  \n";
+	
+	return 0;
+}
